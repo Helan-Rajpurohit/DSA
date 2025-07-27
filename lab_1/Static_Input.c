019ff4ada659e826c0026297b9cf6d0fcb92aa3b
Helan-Rajpurohit <helannetra@gmail.com>
2025-07-27 20:20:42 +0530

    Add files via upload

---
+++ lab_1/Static_Input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int arr[3][3]={{10,54,40},{25,40,39},{20,54,35}};
+    int i,j;
+    printf("3x3 Row Major Matrix\n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\t %d",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
