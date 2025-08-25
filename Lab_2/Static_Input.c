73ef4601d681059af1a36d131a0b3adab639580e
Helan-Rajpurohit <helannetra@gmail.com>
2025-08-25 17:56:11 +0530

    Update Static_Input.c

--- Lab_2/Static_Input.c
+++ Lab_2/Static_Input.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main()
 {
+    // 3x3 array declaration & initialisation
     int arr[3][3]={{10,54,40},{25,40,39},{20,54,35}};
+    
     int i,j;
+
+    //print elements
     printf("3x3 Row Major Matrix\n");
     for(i=0;i<3;i++)
     {
@@ -13,5 +17,6 @@ int main()
         printf("\n");
     }
     return 0;
+
 
 }
